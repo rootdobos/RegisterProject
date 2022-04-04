bb83e4e732248839a9efac88ea51eed91cad26db
rootdobos <49080519+rootdobos@users.noreply.github.com>
2022-04-05 02:44:35 +0300

    added perfect link
    as lowest implemented communication level

--- RegisterProject/BestEffortBroadcast.cs
+++ RegisterProject/BestEffortBroadcast.cs
@@ -5,20 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 using Google.Protobuf;
 using Google.Protobuf.Communication;
+using System.Threading.Tasks;
 namespace RegisterProject
 {
     public class BestEffortBroadcast
     {
-        public BestEffortBroadcast(List<ProcessId> processes)
+        public event EventHandler<BEBDeliverEventArgs> Deliver;
+        public BestEffortBroadcast(List<ProcessId> processes, PerfectLink perfectLink)
         {
             _Processes = processes;
+            _PerfectLink = perfectLink;
+            _PerfectLink.Deliver += DeliverFunction;
         }
-        public void Broadcast(string message)
+        public async void Broadcast(Message message)
         {
             foreach (ProcessId process in _Processes)
-                ;//Send process,message
+            {
+                //Send process,message
+                await _PerfectLink.Send(process, message);
+            }
         }
         //Deliver process message
+        public void DeliverFunction(object sender, PerfectLinkDeliverEventArgs e)
+        {
+
+
+            EventHandler<BEBDeliverEventArgs> handler = Deliver;
+            BEBDeliverEventArgs args = new BEBDeliverEventArgs();
+            BebDeliver bebArgs = new BebDeliver()
+                {Sender=e.DeliverMessage.Sender,
+                Message=e.DeliverMessage.Message};
+            args.DeliverMessage = bebArgs;
+            handler?.Invoke(this, args);
+        }
         List<ProcessId> _Processes;
+        PerfectLink _PerfectLink;
+
+    }
+    public class BEBDeliverEventArgs : EventArgs
+    {
+        public BebDeliver DeliverMessage { get; set; }
     }
 }
---
+++ RegisterProject/PerfectLink.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.Communication;
+using System.Threading.Tasks;
+
+namespace RegisterProject
+{
+     public   class PerfectLink
+    {
+        public event EventHandler<PerfectLinkDeliverEventArgs> Deliver;
+        public async Task Send(ProcessId destination, Message message)
+        {
+
+        }
+        public void DeliverFunction()
+        {
+
+
+            EventHandler<PerfectLinkDeliverEventArgs> handler = Deliver;
+            PerfectLinkDeliverEventArgs args = new PerfectLinkDeliverEventArgs();
+            args.DeliverMessage = null;
+            handler?.Invoke(this, args);
+        }
+        
+    }
+    public class PerfectLinkDeliverEventArgs: EventArgs
+    {
+        public PlDeliver DeliverMessage{ get; set; }
+    }
+}
